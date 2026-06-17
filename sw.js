fd1a4371eb05a39f0a2efa19a3183285ec3e875c
bsboonie <boonstrasemail@gmail.com>
2026-06-17 18:39:32 -0400

    small edits

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'yip-v2';
+const CACHE = 'yip-v3';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => {
   self.skipWaiting();
